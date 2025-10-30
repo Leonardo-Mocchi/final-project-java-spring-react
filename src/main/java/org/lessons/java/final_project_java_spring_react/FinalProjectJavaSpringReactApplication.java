60d3fb0e760c2fd36806f9186dad2fde19325a26
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-10-30 05:31:32 +0100

    first commit

---
+++ src/main/java/org/lessons/java/final_project_java_spring_react/FinalProjectJavaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.final_project_java_spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectJavaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJavaSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/final_project_java_spring_react/FinalProjectJavaSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.final_project_java_spring_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectJavaSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
